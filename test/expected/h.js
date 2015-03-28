d982c09f447d9d2f5228e1744353556972a3a612
tanem <tane.morgan@gmail.com>
2015-03-28 19:09:54 +1300

    Retain comments above module exports (fixes #2)

---
+++ test/expected/h.js
@@ -0,0 +1,11 @@
+var Backbone = require('backbone'),
+  _ = require('underscore');
+
+var FooView = Backbone.View.extend({
+  id: 'foo'
+});
+
+/*
+Some comment.
+*/
+module.exports = FooView;
---
+++ test/fixtures/h.js
@@ -0,0 +1,15 @@
+define(function(require){
+
+  var Backbone = require('backbone'),
+    _ = require('underscore');
+
+  var FooView = Backbone.View.extend({
+    id: 'foo'
+  });
+
+  /*
+  Some comment.
+  */
+  return FooView;
+
+});
