6f81a83994999c3a9e089b8378ea7796b6cede99
tanem <tane.morgan@gmail.com>
2015-03-29 13:06:32 +1300

    Add ability to handle multiple files via a source dir

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,5 +1,31 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
 var recastDeamdify = require('../');
+var minimist = require('minimist');
 
-process.stdin.pipe(recastDeamdify()).pipe(process.stdout);
+var argv = minimist(process.argv.slice(2));
+
+if (argv.h || argv.help) {
+  return showUsage();
+}
+
+var srcDir = argv._.toString();
+var destDir = argv.o;
+
+if (!srcDir.length && !destDir) {
+  return process.stdin.pipe(recastDeamdify()).pipe(process.stdout);
+}
+
+if (srcDir.length && destDir) {
+  return recastDeamdify({
+    srcDir: srcDir,
+    destDir: destDir
+  });
+}
+
+showUsage();
+
+function showUsage() {
+  fs.createReadStream(__dirname + '/usage.txt').pipe(process.stdout);
+}
--- index.js
+++ index.js
@@ -1,220 +1,39 @@
+var readdirp = require('readdirp');
 var through = require('through');
-var recast = require('recast');
-var zip = require('lodash.zip');
-var compose = require('lodash.compose');
+var fs = require('fs');
+var path = require('path');
+var deamdify = require('./lib/deamdify');
+var write = require('./lib/write');
 
-module.exports = function(){
+module.exports = function(options, done){
 
-  var data = '';
-
-  return through(write, end);
-
-  function write(buf) {
-    data += buf;
+  if (!options) {
+    return deamdify();
   }
 
-  function end() {
+  options || (options = {});
+  done || (done = function(){});
 
-    var source = data.toString();
-    var ast = recast.parse(source);
-    var body = ast.program.body[0];
-    var factoryBody = getFactoryBody(body);
-    var transform;
+  var source = readdirp({
+    root: options.srcDir,
+    fileFilter: '*.js'
+  });
 
-    if (isAMD(body)) {
-      transform = compose(
-        scrubVariableLoc,
-        createModuleExport
-      );
+  var counter = 0;
 
-      if (!isSimplifiedCommonJs(body)) {
-        var names = getFactoryParams(body);
-        var values = getDependencies(body);
-        transform = compose(
-          transform,
-          injectNewVariableDeclarations(
-            createNewVariableDeclarations(names, values)
-          )
-        );
-      }
+  source.pipe(through(function(entry){
 
-      ast.program.body = transform(factoryBody);
-      this.queue(recast.print(ast, { quote: 'single' }).code);
+    counter++;
 
-    } else {
-      this.queue(source);
-    }
+    fs.createReadStream(entry.fullPath)
+      .pipe(deamdify())
+      .pipe(write(path.join(options.destDir, entry.parentDir), entry.name))
+      .on('finish', function(){
+        if (--counter) done();
+      });
 
     this.queue(null);
 
-  }
+  }));
 
 };
-
-function isAMD(body) {
-  return body.type === 'ExpressionStatement' &&
-    body.expression.type === 'CallExpression' &&
-    body.expression.callee.name === 'define';
-}
-
-function isSimplifiedCommonJs(body) {
-  return body.expression.arguments
-    .filter(function(argument){
-      return argument.type === 'FunctionExpression';
-    })
-    .map(function(argument){
-      return argument.params;
-    })
-    .reduce(function(prev){
-      return prev;
-    })
-    .some(function(param){
-      return param.name === 'require';
-    });
-}
-
-// This gives the outputted var declaration format we want.
-function scrubVariableLoc(factoryBody) {
-  return factoryBody
-    .map(function(node){
-      if (node.type === 'VariableDeclaration' && node.loc) {
-        delete node.loc;
-      }
-      return node;
-    });
-}
-
-function getDependencies(body) {
-  return body.expression.arguments
-  .filter(function(argument){
-    return argument.type === 'ArrayExpression';
-  })
-  .map(function(argument){
-    return argument.elements;
-  })
-  .reduce(function(prev, curr){
-    return curr || prev;
-  }, [])
-  .map(function(element){
-    return element.value;
-  });
-}
-
-function getFactoryParams(body) {
-  return body.expression.arguments
-  .filter(function(argument){
-    return argument.type === 'FunctionExpression';
-  })
-  .map(function(argument){
-    return argument.params;
-  })
-  .reduce(function(prev){
-    return prev;
-  })
-  .map(function(param){
-    return param.name;
-  });
-}
-
-function getFactoryBody(body) {
-  return body.expression.arguments
-  .filter(function(argument){
-    return argument.type === 'FunctionExpression';
-  })
-  .map(function(argument){
-    return argument.body.body;
-  })
-  .reduce(function(prev){
-    return prev;
-  });
-}
-
-function injectNewVariableDeclarations(newVariableDeclarations) {
-  return function(factoryBody){
-    var result = factoryBody.slice();
-    var variableDeclarationIndex;
-    result.some(function(node, index){
-      if (node.type === 'VariableDeclaration') {
-        variableDeclarationIndex = index;
-        return true;
-      }
-    });
-    result.splice.apply(result, [variableDeclarationIndex, 0].concat(newVariableDeclarations));
-    return result;
-  };
-}
-
-function createModuleExport(factoryBody) {
-  return factoryBody
-  .map(function(node){
-    if (node.type === 'ReturnStatement') {
-      return {
-        type: 'ExpressionStatement',
-        expression: {
-          type: 'AssignmentExpression',
-          operator: '=',
-          left: {
-            type: 'MemberExpression',
-            computed: false,
-            object: {
-              type: 'Identifier',
-              name: 'module'
-            },
-            property: {
-              type: 'Identifier',
-              name: 'exports'
-            }
-          },
-          right: node.argument
-        }
-      };
-    } else {
-      return node;
-    }
-  });
-}
-
-function getFactoryReturnArgument(factoryBody) {
-  return factoryBody
-  .filter(function(node){
-    return node.type === 'ReturnStatement';
-  })
-  .map(function(returnStatement){
-    return returnStatement.argument;
-  })
-  .reduce(function(prev){
-    return prev;
-  });
-}
-
-function createNewVariableDeclarations(names, values) {
-  return zip(names, values)
-    .map(function(pair){
-      return createNewVariableDeclaration(pair[0], pair[1]);
-    });
-}
-
-function createNewVariableDeclaration(name, value) {
-  return {
-    type: 'VariableDeclaration',
-    kind: 'var',
-    declarations: [{
-      type: 'VariableDeclarator',
-      id: {
-        type: 'Identifier',
-        name: name
-      },
-      init: {
-        type: 'CallExpression',
-        callee: {
-          type: 'Identifier',
-          name: 'require'
-        },
-        arguments: [{
-          type: 'Literal',
-          value: value
-        }]
-      }
-    }]
-  };
-}
